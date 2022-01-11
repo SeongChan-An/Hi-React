b69a3c38eda52c5260e779d067a11ac67704b965
tjscks0510 <tjscks0510@gmail.com>
2022-01-11 14:44:21 +0900

    [Seongchan, ticket-#2] [Title] Markup Login, Register page [Description] Markup Login, Register page [Relevant] LoginPage.js, RegisterPage.js

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function RegisterPage() {
-  return <div></div>;
+  return (
+    <div className="auth-wrapper">
+      <h1>회원가입.</h1>
+      <form>
+        <input
+          placeholder="Apple 아이디"
+          type="email"
+          name="email"
+          value=""
+        ></input>
+        <input
+          placeholder="Apple 암호"
+          type="password"
+          name="password"
+          value=""
+        ></input>
+        <button type="submit">회원가입.</button>
+        <Link to="login" style={{ color: "gray", textDecoration: "none" }}>
+          이미 Apple ID가 있다면? 지금 로그인.{" "}
+        </Link>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;
