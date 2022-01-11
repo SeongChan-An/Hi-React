bbf8c91b27e70a507c24ca1860629dc4b52e304b
tjscks0510 <tjscks0510@gmail.com>
2022-01-11 14:44:21 +0900

    [Seongchan, ticket-#2] [Title] Markup Login, Register page [Description] Markup Login, Register page [Relevant] LoginPage.js, RegisterPage.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
+import axios from "axios";
+import "./css/auth.css";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Clayful from "clayful/client-js";
-import axios from "axios";
 const ClayfulConfig = require("./Config/clayfulToken.json");
 
 Clayful.config({
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
-  return <div></div>;
+  return (
+    <div className="auth-wrapper">
+      <h1>로그인.</h1>
+      <form>
+        <input
+          placeholder="Apple 아이디"
+          type="Apple ID"
+          name="email"
+          value=""
+        />
+        <input
+          placeholder="Apple PW"
+          type="password"
+          name="password"
+          value=""
+        />
+        <p>
+          Apple ID는 iTunes, App Store, iCloud에 로그인할 때 사용하는 이메일
+          주소입니다.
+        </p>
+        <button type="submit">로그인.</button>
+        <Link to="login" style={{ color: "gray", textDecoration: "none" }}>
+          Apple ID가 없으신가요? 지금 생성.{" "}
+        </Link>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
